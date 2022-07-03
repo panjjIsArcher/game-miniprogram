908578e39b1c961712fbc3fca67ec5382523d549
panjiajie <251907882@qq.com>
2022-07-03 16:18:42 -0700

    text

--- src/class/pixi.js
+++ src/class/pixi.js
@@ -2,9 +2,34 @@ import * as PIXI from "pixi.js"
 export default class Game {
     constructor(config) {
         const { width, height, name } = config
-        const game = new PIXI.Application({width,height})
+        const game = new PIXI.Application({ width, height })
         const root = document.querySelector(`#${name}`)
         root.appendChild(game.view)
+        this.PIXI = PIXI
         this.game = game
+    }
+    // 创建文本
+    createText(str, config) {
+        const { left, top } = config
+        const style = new this.PIXI.TextStyle({
+            fontFamily: 'Arial', // 字体
+            fontSize: 20, // 
+            fontWeight: 'bold',
+            stroke: "#fff",
+            wordWrap: true,
+            fill: ['#fff'],
+            // lineHeight: 30,
+            align:'left',
+            ...config
+        })
+        const text = new this.PIXI.Text(str, style)
+        // if (left) {
+            // const width = window.innerWidth - 2 * left
+            // text.width = width
+        // }
+        text.position.set(left, top)
+        text.name = config.name
+        this.game.stage.addChild(text)
+
     }
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,18 +5,60 @@
 import Game from '@/class/pixi'
 import '../css/index.less'
 export default {
+    data() {
+        return {
+            game: null,
+            tips:
+                `在『母体』中，你所见的\n一切都不是真实
+
+选择蓝色药丸，明天早上你会\n在你的床上醒来，继续你的生活
+
+选择红色药丸，我会带你去看\n这个世界的真相`
+        }
+    },
     methods: {
+        // 初始化
         init() {
             const config = {
                 width: window.innerWidth,
                 height: window.innerHeight,
-                name:'index'
+                name: 'index'
             }
             const game = new Game(config)
+            this.game = game
+            window.game = game
+        },
+        text(str) {
+            const config = {
+                left: 50,
+                top: 150,
+                stroke: 'rgb(4, 60, 4)',
+                fill: ['rgb(4, 60, 4)'],
+                fontSize: 20,
+                name: 'tips'
+            }
+            this.game.createText(str, config)
+        },
+        updateText(tips) {
+            let str = ''
+            for (let i = 0; i < tips.length; i++) {
+                let timeout = setTimeout(() => {
+                    let letter = tips[i]
+                    str += letter
+                    const prev = this.game.game.stage.children.find(e => e.name === 'tips')
+                    prev.text = tips[i] ? str + '_' : str
+                    if (i >= tips.length) {
+                        clearTimeout(timeout)
+                    }
+                }, 200 * i)
+            }
         }
     },
-   mounted() {
+    mounted() {
         this.init()
+        // 文字提示
+        this.text('_')
+        this.updateText(this.tips)
     }
 }
 </script>
